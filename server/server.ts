a62a109a9a0eae4bdf4c6787c6875f334ae8061a
Lukas Knöpfel <shylux@gmail.com>
2020-02-02 16:25:04 +0100

    Fix bug where server fails when no tier is passed.

--- server/server.ts
+++ server/server.ts
@@ -19,7 +19,7 @@ app.use(cors());
 app.use(express.static('.'));
 
 app.get('/match', (req, res) => {
-  const tier = TIERS_UPPER.indexOf(req.query.tier.toUpperCase()) > -1 ? req.query.tier.toUpperCase() : 'GOLD';
+  const tier = TIERS.indexOf(req.query.tier) > -1 ? req.query.tier.toUpperCase() : 'GOLD';
   const division = DIVISIONS.indexOf(req.query.division) > -1 ? req.query.division : 'I';
 
   res.setHeader('Content-Type', 'application/json');
